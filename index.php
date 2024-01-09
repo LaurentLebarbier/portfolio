621b99f4ccab5e29b0d5fb86e18790b065b7553d
“Laurent <laurentlebarbier70@gmail.com>
2024-01-09 10:23:21 +0100

    Mise a jour

--- index.php
+++ index.php
@@ -38,7 +38,6 @@
         <a href="index.php"><img src="logos/LLogo.png" alt="logo Laurent Lebarbier"></a>
         <h1>Laurent Lebarbier</h1>
         <h2>Développeur web</h2>
-        <h2>Front-end</h2>
       </div>
 
       <div id="nav_et_rs">
